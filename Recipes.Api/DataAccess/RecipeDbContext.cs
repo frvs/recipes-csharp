8b73fbd1af72dce7b0a64e0431f747e6d17a64b7
Frois <frois.dev@gmail.com>
2022-04-29 02:47:30 -0300

    setup entity framework

---
+++ Recipes.Api/DataAccess/RecipeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recipes.Api.Models;
+
+namespace Recipes.Api.DataAccess
+{
+    public class RecipeDbContext : DbContext
+    {
+        public RecipeDbContext(DbContextOptions<RecipeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<Tool> Tools { get; set; }
+        public DbSet<Food> Foods { get; set; }
+    }
+}
